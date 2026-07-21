338d10d0651a361e0048796855c2dba4a41ad99a
Programmeerkat <danieltennapel@gmail.com>
2026-07-21 21:49:43 +0200

    fixed sql credentials, added .env.example

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -2,9 +2,14 @@ import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  user: process.env.DB_USER,
+  user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT as unknown as number,
+  ssl: {
+    minVersion: 'TLSv1.2',
+    rejectUnauthorized: true
+  },
 });
 
 export default pool;
